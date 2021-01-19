32cb6257346346decdb441bbb1f8240839d14f00
DEM025 <deepak.emani@maersk.com>
2021-01-19 13:51:10 +0000

    first reactive end point

---
+++ src/main/java/com/example/springreactivesampleappdee/SpringReactiveSampleAppDeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.springreactivesampleappdee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveSampleAppDeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactiveSampleAppDeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springreactivesampleappdee/SpringReactiveSampleAppDeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springreactivesampleappdee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveSampleAppDeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
